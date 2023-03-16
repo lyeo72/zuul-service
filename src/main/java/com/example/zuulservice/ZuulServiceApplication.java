c5e1cff7ddade0fc042393321563625d7d2ad0eb
yeojin <lyeo7272@gmail.com>
2023-03-16 20:08:45 +0900

    change

--- src/main/java/com/example/zuulservice/ZuulServiceApplication.java
+++ src/main/java/com/example/zuulservice/ZuulServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.zuulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServiceApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/zuulservice/ZuulServiceApplicationTests.java
+++ src/test/java/com/example/zuulservice/ZuulServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ZuulServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
